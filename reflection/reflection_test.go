e7c9ddf08ae76497e7002d9a240996a09a71d96c
hukma <hukmaulil23@gmail.com>
2025-09-07 14:26:58 +0700

    membuat reflection directory

---
+++ reflection/reflection_test.go
@@ -0,0 +1,20 @@
+package reflection
+
+import "testing"
+
+func TestWalk(t *testing.T) {
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function call, got %d want %d", len(got), 1)
+	}
+}
